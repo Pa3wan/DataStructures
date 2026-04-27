4c29a814c856e35f2e44ee72f4bab1dcd5422a33
pa3wan <stanpandey@gmail.com>
2026-04-27 22:55:09 +0545

    Added Movie struct, MovieRating enum, and TVShow class with constructor and ToString override.

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -4,12 +4,46 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.ReadKey();
+            Movie deadpool3;
+            deadpool3.title = "Deadpool & Wolverine";
+            deadpool3.releaseYear = 2024;
+            deadpool3.price = 19.99;
+            deadpool3.rating = MovieRating._15;
 
-            float someFloat = 3.14f;
-            Console.WriteLine(someFloat.ToString("0.000"));
+            Console.WriteLine(deadpool3.title + " (" + deadpool3.releaseYear + ") Rated " + deadpool3.rating);
+
             Console.ReadKey();
+            Console.WriteLine("\n\n");
+            //Console.Clear();
+
+            List<TVShow> shows = new List<TVShow>();
+            shows.Add(new TVShow("The Lord of the Rings: The Rings of Power", 2022, 0, MovieRating._15));
+            shows.Add(new TVShow("The Boys", 2019, 7.99, MovieRating._18));
+            shows.Add(new TVShow("Ben and Holly's Little Kingdom", 2009, 12.45, MovieRating._U));
+
+            for (int i = 0; i < shows.Count; i++)
+            {
+                Console.WriteLine(shows[i].ToString());
+            }
+
+            Console.ReadKey();
+        }
+
+        public enum MovieRating
+        {
+            _U,
+            _12A,
+            _12,
+            _15,
+            _18
+        }
+
+        public struct Movie
+        {
+            public string title;
+            public int releaseYear;
+            public double price;
+            public MovieRating rating;
         }
     }
 }
