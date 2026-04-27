4089735820c83beb3a0b6e6ebc98c0a0b0cd317b
pa3wan <stanpandey@gmail.com>
2026-04-27 06:23:27 +0545

    Add project files.

---
+++ DataStructures/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace DataStructures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadLine();
+        }
+    }
+}
